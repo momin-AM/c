8d8a883b489824f10282724063f3b751ac229dc4
Momin <sk4549151@gmail.com>
2025-10-02 12:05:04 +0530

    Create ch-6.c

---
+++ exercise/ch-6.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+//(B-a)multiplication table
+
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     for(int i=1;i<=10;i++){
+//         printf("%d*%d=%d\n",n,i,i*n);
+//     }
+//     return 0;
+// }
+
+//(B-c) read 10 sets f p,r,n,q and get compound interest
+// #include <math.h>
+// int main(){
+//     float p,r,n,q;
+//     for(int i=0;i<10;i++){
+//         printf("enter p r n q");
+//         scanf("%f %f %f %f",&p,&r,&n,&q);
+//         float temp=p*(1+r/q);
+//         float a=pow(temp,n*q);
+//         printf("compound : %.2f inr\n",a);
+
+//     }
+// }
+
+//(B-d) natural logarithm series 
+// #include <math.h>
+// int main(){
+//     float x;
+//     scanf("%f",&x);
+//     float number=(x-1)/x;
+//     float sum=number;
+//     for(int i=2;i<=7;i++){
+//         sum+=(1.0/i)*pow(number,i);
+//     }
+//     printf("sum of first 7 terms is : %f\n",sum);
+//     return 0;
+
+// }
+
