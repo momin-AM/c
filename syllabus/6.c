af8560df7af00e8a5e2686a4f528c304bb85247e
Momin <sk4549151@gmail.com>
2025-11-04 18:54:38 +0530

    Add program to calculate circumference and area

---
+++ syllabus/6.c
@@ -0,0 +1,9 @@
+//read radius get circumferance and area
+#include <stdio.h>
+
+int main(){
+    int r;
+    scanf("%d",&r);
+    printf("circumferance is : %.3f\narea is : %.3f\n",2*3.14*r, 3.14*r*r);
+    return 0;
+}
