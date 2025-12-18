50af0bbb6060a6069fee4944dd4906db4828b5bf
Momin <sk4549151@gmail.com>
2025-12-18 16:56:15 +0530

    Update ch2-Instructions.c

--- exercise/ch2-Instructions.c
+++ exercise/ch2-Instructions.c
@@ -103,3 +103,40 @@ int main(){
     return 0;
 
 }
+
+//TRIANGLE AREA CALCULATE
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float a,b,c,s,area=0;
+        printf("enter length of 3 sides of triangle : ");
+        scanf("%f %f %f", &a, &b, &c);
+        s = (a + b + c) / 2;
+        area = sqrt(s * (s - a) * (s - b) * (s - c));
+        printf("area is : %.3f\n", area);
+        return 0;
+}
+
+//REVERSE A NUMBER
+
+int main(){
+    int userInput,arr[10],digits=1,result=0;
+    printf("enter number : ");
+    scanf("%d",&userInput);
+    int temp=userInput;
+    while((userInput/10)!=0){
+        userInput/=10;
+        digits+=1;
+    }
+    userInput=temp;
+    printf("reversed number : ");
+    for(int i=0;i<digits;i++){
+        arr[i]=userInput%10;
+        userInput/=10;
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
+
