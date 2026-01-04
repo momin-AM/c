32b34e2f41ee3e1a3a7aa65c781a434c28b70bd8
Momin <sk4549151@gmail.com>
2026-01-04 20:56:28 +0530

    Update ch-16.c

--- exercise/ch-16.c
+++ exercise/ch-16.c
@@ -163,37 +163,117 @@ int main()
 //     return 0;
 // }
 
-//convert any number to string form(12345 = twelve thousand 3 hundred forty five)
+//convert any number to string form(12345 = twelve thousand three hundred forty five)
 
-char* numToStr(int num){
-    char *tillTwenty[]={"","one","two","three","four","five",
-                        "six","seven","eight","nine","ten",
-                        "eleven","twelve","thirteen","fourteen","fifteen",
-                        "sixteen","seventeen","nineteen","twenty"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+char* numToStr(long long num) {
+    static char res[300];
+    res[0] = '\0';
+
+    char *ones[] = {
+        "", "one","two","three","four","five",
+        "six","seven","eight","nine","ten",
+        "eleven","twelve","thirteen","fourteen","fifteen",
+        "sixteen","seventeen","eighteen","nineteen"
     };
-    char res[200]="";
-    int pref=0;
-    char *prefixes[]={"","hundred","thousand","lakh","crore"};
-    int hunMod;
-    while(num!=0){
-        hunMod=num%100;
-        if(hunMod<21){
-            strcat(res,tillTwenty[hunMod]);
-        }else{
-            strcat(res,tillTwenty[hunMod/10]);
-            strcat(res,"ty\0");
-            strcat(res,tillTwenty[hunMod%10]);
-        }
-        strcat(res,prefixes[pref]);
-        pref++;
+
+    char *tens[] = {
+        "", "", "twenty","thirty","forty",
+        "fifty","sixty","seventy","eighty","ninety"
+    };
+
+    if (num == 0) {
+        strcpy(res, "zero");
+        return res;
     }
+    if (num >= 20000000000) {
+        strcat(res, tens[num / 10000000000]);
+        strcat(res, " \0");
+        num %= 20000000000;
+        if(num<1000000000) strcat(res, "Billion ");
+    }
+
+    if (num >= 1000000000) {
+        strcat(res, ones[num / 1000000000]);
+        strcat(res, " Billion ");
+        num %= 1000000000;
+    }
+    if (num >= 200000000) {
+        strcat(res, tens[num / 100000000]);
+        strcat(res, " \0");
+        num %= 200000000;
+        if(num<10000000) strcat(res, "crore ");
+    }
+
+    if (num >= 10000000) {
+        strcat(res, ones[num / 10000000]);
+        strcat(res, " crore ");
+        num %= 10000000;
+    }
+    if (num >= 2000000) {
+        strcat(res, tens[num / 1000000]);
+        strcat(res, " \0");
+        num %= 2000000;
+        if(num<100000) strcat(res, "lakh ");
+    }
+
+    if (num >= 100000) {
+        strcat(res, ones[num / 100000]);
+        strcat(res, " lakh ");
+        num %= 100000;
+    }
+    if (num >= 20000) {
+        strcat(res, tens[num / 10000]);
+        strcat(res, " \0");
+        num %= 20000;
+        if(num<1000) strcat(res, "thousand ");
+    }
+
+    if (num >= 1000) {
+        strcat(res, ones[num / 1000]);
+        strcat(res, " thousand ");
+        num %= 1000;
+    }
+
+    if (num >= 100) {
+        strcat(res, ones[num / 100]);
+        strcat(res, " hundred ");
+        num %= 100;
+    }
+
+    if (num >= 20) {
+        strcat(res, tens[num / 10]);
+        strcat(res, " ");
+        num %= 10;
+    }
+
+    if (num > 0) {
+        strcat(res, ones[num]);
+    }
+
     return res;
 }
 
-int main(){
-    int num=123;
-    char res[200];
-    res=numToStr(num);
-    printf("%s\n",res);
+int main() {
+    long long int num;
+    while (1)
+    {
+        printf("enter number: ");
+        scanf("%lld",&num);
+        if(num==-1) break;
+        else{
+            char *text = numToStr(num);
+            for (int i = 0; text[i]; i++) {
+                putchar(text[i]);
+                fflush(stdout);
+                usleep(50000); // 50 ms
+            }
+            printf("\n");
+        }
+    }
     return 0;
 }
