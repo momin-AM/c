fd0f1d1584cecd044f1800dd13f8792a8adcc8a0
Momin <sk4549151@gmail.com>
2026-02-13 19:30:32 +0530

    Update ch-21.c

--- exercise/ch-21.c
+++ exercise/ch-21.c
@@ -30,4 +30,36 @@ void showWin(char *arr[],int game){
         printf("\nchampion of champions\n");
     }
 }
-
+//A-b
+#include <stdio.h>
+int main(){
+    char types[][20]={
+                "canine",
+                "feline",
+                "cetacean",
+                "marsupial"
+    };
+    char eatType[][20]={
+        "herbivore",
+        "carnivore"
+    };
+    struct animal{
+        char name[50];
+        int type;
+    };
+    struct animal a={"ocelot",18};
+    int mask;
+    int typeInd=0,eatTypeInd;
+    int temp;
+    for(int i=3;i>=0;i--){
+        mask=1<<i;
+        if(temp=a.type & mask!=0){
+            typeInd=i;
+            break;
+        }
+    }
+    mask=1<<4;
+    eatTypeInd=a.type & mask!=0?1:0;
+    printf("type : %s\neat type : %s\n",types[typeInd],eatType[eatTypeInd]);
+    return 0;
+}
